d44b51b45eef29039241ade08469c1eeaf05382a
Kayro <kayro012014@gmail.com>
2024-09-10 12:52:02 +0800

    toggle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Koze Cafe</title>
 
     <!-- Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
+
+    
     <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
 </head>
 <body>
@@ -18,15 +21,18 @@
         <div class="container">
             <a class="navbar-brand" href="#" style="color: black; font-weight: bold">Koze Cafe</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+               <span class="material-symbols-outlined">
+                pets
+                </span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0" >
                     <li class="nav-item">
-                        <a class="nav-link" href="#home">HOME</a>
+                        <a class="nav-link" href="#carouselExampleCaptions">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#aboutus">ABOUT US!</a>
+                        <a class="nav-link" href="#aboutus">ABOUT US</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#amenities">AMENITIES</a>
@@ -41,7 +47,7 @@
     </nav>
 
     <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
+        <div class="carousel-indicators" id="aboutus">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -81,7 +87,7 @@
 
 
      <!-- about services --> 
-        <section id="about" class="about section-padding">
+        <section id="#about" class="about section-padding">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 col-md-12 col-12">
@@ -109,20 +115,29 @@
 
         <!--- amenities Section --->
 
-        <section id="services" class="services section-padding">
+                 <section id="amenities" class="amenities section-padding">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="section-header text-center pb-5">
+
+                    <div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
+                        <div class="about-text">
                             <h2>Our Amenities</h2>
-                            <p>At Koze Cafe, you can enjoy a range of amenities including free high-speed Wi-Fi, <br>
+                            <p>At Koze Cafe, you can enjoy a range of amenities including free high-speed Wi-Fi,
                                  cozy seating areas, and a selection of delicious beverages, 
-                                <br>all while spending time with our friendly resident cats.</p>
+                                all while spending time with our friendly resident cats.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-12">
+                        <div class="cat-img">
+                            <img src="assets/pictures/koze_cat.jpg" alt="About" class="img-fluid">
                         </div>
                     </div>
                 </div>
+            </div>
+        </section>
 
-                                <div class="row justify-content-center">
+
+                <div class="row justify-content-center">
                     <div class="col-12 col-md-8 col-lg-4 d-flex justify-content-center">
                         <div class="card text-white text-center bg-dark pb-2 custom-card-width">
                             <div class="card-body">
@@ -140,7 +155,7 @@
         </section>
 
       <!--- Contact -->
-      <section id="contact" class="contact section-padding">
+      <section id="contactus" class="contact section-padding">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
@@ -167,8 +182,9 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Q5T1XwooxJrrH5EYCGJNLsAxZ8HD/z6cAF0b1vXpVV4L9L4soJqaYPzXj6W3urJb" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-1fRBx07U5OELlzWTzNljESrcpbLGd22AKQwEp+Zg4yB5FFLRkIkJmc6HlXK6o9j5" crossorigin="anonymous"></script>
+
     <script>
         document.addEventListener('click', function (e) {
             if (!e.target.closest('.navbar-collapse') && !e.target.closest('.navbar-toggler')) {
