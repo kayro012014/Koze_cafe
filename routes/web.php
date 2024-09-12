facebb8fb52fef8e9cab14205a5c7d97ab7b95c9
Kayro <kayro012014@gmail.com>
2024-09-12 22:09:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/admin', function () {
-//     return view('admin.dashboard');
-// })->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
