f1749b59ec9f81c8c306f2bc365ec90fcf5ef55b
narutohiek <133133609+narutohiek@users.noreply.github.com>
2024-09-24 21:09:15 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Inventory;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
